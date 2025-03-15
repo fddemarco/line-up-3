f67cb0b98de49f899936d73f84ad6c92c7363f17
FrancoD <franco.demarco3400@gmail.com>
2025-03-15 17:18:33 -0300

    feat: add basic board

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+
+const TURNS = {
+  X: "x",
+  O: "o"
+}
+
+const board = Array(9).fill(null)
+
+
+
 function App() {
-  return <h1>Tic tac toe</h1>
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div>
+                <span>
+                  {index}
+                </span>
+              </div>
+            )
+          }
+        )
+        }
+      </section>
+    </main>
+)
 }
 
 export default App
